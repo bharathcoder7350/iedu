4e40758b1af5d434e99328908c7b9d1bf6ba1e7c
bharathcoder7350 <130210092+bharathcoder7350@users.noreply.github.com>
2023-05-07 12:10:13 +0530

    commit changes to ideu main

---
+++ index.js
@@ -0,0 +1,10 @@
+let button = document.getElementById('btn');
+let topg = document.getElementById('top');
+
+button.addEventListener('click', ()=>{
+    window.location.href = "/assets/project/iEdu/book.html";
+});
+
+topg.addEventListener('click', ()=>{
+    window.location.href = "/assets/project/index.html#";
+});
